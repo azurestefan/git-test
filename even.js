0a9281b2897ced531d2c31154fd2951168466edf
jmh <azurestefan@naver.com>
2020-06-12 14:39:59 +0900

    first

---
+++ even.js
@@ -0,0 +1,12 @@
+function is_even(number) {
+
+	let calculate = parseInt(number) %2;
+
+	if(calculate ===1){
+		return "not an even number";
+	}else if (calculate ===0) {
+		return "it's an even nuber";
+	}
+}
+
+console.log(is_even(11));
